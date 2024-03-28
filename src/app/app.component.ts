cab6256de890a7ad0b39f79fa180843c8f2e860c
Zoltan Halasz <zoltan.halasz@qubiz.ro>
2024-03-28 10:14:31 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reactive-forms';
+  myForm: FormGroup = new FormGroup({});
+  
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    // Initialize the form with two fields
+    this.myForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required]
+    });
+  }
+  // Submit handler
+  onSubmit() {
+    if (this.myForm.valid) {
+      console.log('Form submitted successfully!');
+      console.log('First Name:', this.myForm.value.firstName);
+      console.log('Last Name:', this.myForm.value.lastName);
+    }
+  }
 }
